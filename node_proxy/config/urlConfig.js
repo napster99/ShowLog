583a8aa766825f1817afcfdfc36d62e8ac66f2a0
章晓健 <809270105@qq.com>
2016-10-10 17:16:14 +0800

    er

--- node_proxy/config/urlConfig.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports.URLConfig = {
-  noLogin : ['/main/auth/send_code', '/main/auth/login'],
-  noAgent : ['/main/auth/info','/main/auth/send_code', '/main/auth/login', '/main/user/list','/main/user/delete','/main/user/banned','/main/user/add','/main/user/info','/main/user/create','/main/role/list', '/main/role/add', '/main/role/add_multi', '/main/role/get', '/main/role/banned', '/main/role/delete', '/main/role/priv_list', '/main/user/role_list','/main/role/region','/main/auth/logout']
-}
